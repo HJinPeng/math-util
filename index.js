3ceb59fd1fed40f9ddd296ac671ca6579961f889
jinpengh <jinpengh@digitalgd.com.cn>
2021-10-08 20:23:39 +0800

    feat: 添加eslint指令

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-;(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-      global.moduleName = factory()
-}(this, (function () {
-  var test = {
-    sayHi: function () {
-      console.log('hi, this is the version of 1.0.4, fix it in 1.0.5');
-    },
-    sayBye: function () {
-      console.log('Bye');
-    }
-  };
+// ;(function (global, factory) {
+//   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+//     typeof define === 'function' && define.amd ? define(factory) :
+//       global.moduleName = factory()
+// }(this, (function () {
+//   var test = {
+//     sayHi: function () {
+//       console.log('hi, this is the version of 1.0.4, fix it in 1.0.5');
+//     },
+//     sayBye: function () {
+//       console.log('Bye');
+//     }
+//   };
 
-  return test
-})))
+//   return test
+// })))
+console.log('666');
